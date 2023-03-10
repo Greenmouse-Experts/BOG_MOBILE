fec3f74ceb4d7858fb65a939a4ad8070b788047c
Ekojode <ekojodeoma@gmail.com>
2023-03-10 15:01:02 +0100

    Update main.dart
    test

--- bog/lib/main.dart
+++ bog/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:bog/routes/app_pages.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 
